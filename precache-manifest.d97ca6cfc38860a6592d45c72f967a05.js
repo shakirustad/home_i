4bdf5be0df5eb74187128e8f65e099b2bfcc0e22
shakirustad <ustadhismailgunia@gmail.com>
2022-04-19 19:16:21 +0300

    Updates

---
+++ precache-manifest.d97ca6cfc38860a6592d45c72f967a05.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98aa73ccaa2dd9bcd09fb6d314035f5e",
+    "url": "/home_i/index.html"
+  },
+  {
+    "revision": "678f11b0bf1982bda925",
+    "url": "/home_i/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3ebf362894a7c3b8827a",
+    "url": "/home_i/static/css/main.bca54414.chunk.css"
+  },
+  {
+    "revision": "678f11b0bf1982bda925",
+    "url": "/home_i/static/js/2.089a716d.chunk.js"
+  },
+  {
+    "revision": "ee9159364797373f954fc6d7febc7fb5",
+    "url": "/home_i/static/js/2.089a716d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ebf362894a7c3b8827a",
+    "url": "/home_i/static/js/main.ba651b39.chunk.js"
+  },
+  {
+    "revision": "fd2559f4825c8d089483",
+    "url": "/home_i/static/js/runtime-main.e12ac657.js"
+  },
+  {
+    "revision": "871c13fce32480de59e3ee80260cbe49",
+    "url": "/home_i/static/media/resume.871c13fc.pdf"
+  }
+]);
